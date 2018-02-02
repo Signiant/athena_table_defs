c6e7eb573913d3ee25892abef012963c92877fe6
Dave North <dnorth98@gmail.com>
2018-02-02 08:00:01 -0500

    Update table def to work with glue

--- papertrail.sql
+++ papertrail.sql
@@ -1,18 +1,20 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS papertrail.papertrail_logs (
- 'id' bigint,
- 'generated_at' string,
- 'received_at' string,
- 'source_id' bigint,
- 'source_name' string,
- 'source_ip' string,
- 'facility_name' string,
- 'severity_name' string,
- 'program' string,
- 'message' string 
+CREATE EXTERNAL TABLE IF NOT EXISTS papertrail_logs (
+ id bigint,
+ generated_at string,
+ received_at string,
+ source_id bigint,
+ source_name string,
+ source_ip string,
+ facility_name string,
+ severity_name string,
+ program string,
+ message string 
 )
 PARTITIONED BY (dt string)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
-WITH SERDEPROPERTIES (
- 'serialization.format' = ‘ ‘,
- 'field.delim' = ' '
-) LOCATION 's3://BUCKET/prod/logs/<MY PAPERTRAIL CUST NUM>/'
+ROW FORMAT DELIMITED 
+  FIELDS TERMINATED BY '\t' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://BUCKET/prod/logs/<MY PAPERTRAIL CUST NUM>/'
